48c6f5fe486105990a266e51b3297404e762563b
Aldiyar <aissenbayev@one.kz>
2023-11-11 00:19:55 +0600

    Fix DTO classes with only one field by removing @AllArgsConstructor (this annotation erases no-args constructor which must exist for DTO classes with only one field)

--- src/main/java/kz/iitu/edu/activity/monitoring/dto/activity/request/ActivityStatusUpdateReq.java
+++ src/main/java/kz/iitu/edu/activity/monitoring/dto/activity/request/ActivityStatusUpdateReq.java
@@ -1,11 +1,8 @@
 package kz.iitu.edu.activity.monitoring.dto.activity.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
-
 public class ActivityStatusUpdateReq {
     private String status;
 }
--- src/main/java/kz/iitu/edu/activity/monitoring/dto/activity/request/ActivityUpdateByTranslatorReq.java
+++ src/main/java/kz/iitu/edu/activity/monitoring/dto/activity/request/ActivityUpdateByTranslatorReq.java
@@ -1,10 +1,8 @@
 package kz.iitu.edu.activity.monitoring.dto.activity.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ActivityUpdateByTranslatorReq {
     private String targetTitle;
 }
